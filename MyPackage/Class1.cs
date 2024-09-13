1ebc4099af98f14c2ada7c341ca89bfa45a3a7a3
Muaaz Butt <muaazbutt585@gmail.com>
2024-09-13 11:08:00 +0500

    test program

---
+++ MyPackage/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MyPackage {
+    public class Math {
+        public static int Area(int length_in_feet, int width_in_feet) {
+            return length_in_feet * width_in_feet / 4;  
+        }
+    }
+}
