dd8d0dd98aa0775ce80706460301ed75f3915c6b
Muaaz Butt <muaazbutt585@gmail.com>
2024-09-19 19:08:52 +0500

    count number of words and character in file

---
+++ count_char_word_in_file/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace count_char_word_in_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "dummyData.txt";
+
+            WriteDummyDataToFile(filePath);
+
+            ReadAndDisplayFileData(filePath);
+        }
+
+        static void WriteDummyDataToFile(string filePath)
+        {
+            string[] lines = { "Muaaz Butt" };
+
+            File.WriteAllLines(filePath, lines);
+
+            Console.WriteLine("Dummy data has been written successfully");
+        }
+
+        static void ReadAndDisplayFileData(string filePath)
+        {
+            string fileContent = File.ReadAllText(filePath);
+
+            int numberOfCharacters = fileContent.Length;
+            int numberOfWords = fileContent.Split(new[] { ' ', '\n', '\t', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+            
+            Console.WriteLine($"Character count: {numberOfCharacters}");
+            Console.WriteLine($"Word count: {numberOfWords}");
+        }
+    }
+
+}
